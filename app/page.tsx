43fdb7ed0f0fe5136c6443359cf94b9f3d2eb65a
Stanislav Pika <stanley6631@gmail.com>
2025-06-14 15:46:03 +0200

    wip: camera access, add test products

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use, useState } from "react";
+import React, { use, useState, useRef } from "react";
 import Image from "next/image";
 import EbayProductsList from "@/components/products/EbayProductsList";
 import axios from "axios";
@@ -19,6 +19,9 @@ const HomePage = () => {
   const [description, setDescription] =
     useState<GptProductObjectResponse | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
+  const [cameraActive, setCameraActive] = useState<boolean>(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const { data: products, isLoading: productsLoading } = useProducts(
     description as GptProductObjectResponse
   );
@@ -66,9 +69,53 @@ const HomePage = () => {
     }
   };
 
+  const startCamera = async () => {
+    setCameraActive(true);
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+      }
+    } catch (err) {
+      console.error("Error accessing camera:", err);
+      setCameraActive(false);
+    }
+  };
+
+  const captureImage = () => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+
+      if (context) {
+        context.drawImage(
+          videoRef.current,
+          0,
+          0,
+          canvasRef.current.width,
+          canvasRef.current.height
+        );
+        canvasRef.current.toBlob(async (blob) => {
+          if (blob) {
+            const file = new File([blob], "captured-image.jpg", {
+              type: "image/jpeg",
+            });
+            const optimizedFile = await resizeImage(file);
+            if (optimizedFile) {
+              setSelectedFile(optimizedFile);
+              setPreviewUrl(URL.createObjectURL(optimizedFile));
+              setDescription(null);
+              setCameraActive(false);
+            }
+          }
+        }, "image/jpeg");
+      }
+    }
+  };
+
   return (
     <div className="space-y-4 p-4 max-w-md mx-auto flex flex-col items-center">
-      {!selectedFile && (
+      {!selectedFile && !cameraActive && (
         <>
           <Image
             src={"/robot/default.png"}
@@ -76,20 +123,46 @@ const HomePage = () => {
             width={200}
             alt="Your buddy estimator"
           ></Image>
-          <Button className="w-full relative" variant={"default"}>
+          {/* <Button
+            className="w-full relative"
+            variant={"default"}
+            onClick={startCamera}
+          >
+            <CameraIcon className="mr-2" />
+            Open Camera
+          </Button> */}
+          <Button
+            className="w-full relative relative z-100"
+            variant={"default"}
+          >
             <CameraIcon className="mr-2" />
             Take or upload a photo of your product
             <Input
               type="file"
               onChange={handleFileChange}
               accept="image/*"
-              className="fixed opacity-0 top-[72px] left-0 w-full h-[calc(100vh-134px)] cursor-pointer"
+              className="fixed opacity-0 top-[72px] left-0 w-full h-[calc(100vh-134px)] z-50 cursor-pointer"
             />
           </Button>
           <p className="text-sm text-center text-gray-500">
             For better results, try to take a picture with as much detail as
-            posiible.
+            possible.
           </p>
+        </>
+      )}
+
+      {cameraActive && (
+        <>
+          <video ref={videoRef} className="w-full h-auto rounded mb-4" />
+          <canvas
+            ref={canvasRef}
+            className="hidden"
+            width={300}
+            height={300}
+          ></canvas>
+          <Button className="w-full" onClick={captureImage}>
+            Capture Image
+          </Button>
         </>
       )}
 
